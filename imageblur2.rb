8c201866936619a28a5c08e89aaa710c24417613
Tonia Hurst <51763259+toniahurst@users.noreply.github.com>
2019-09-12 10:49:07 -0700

    Update imageblur2.rb

--- imageblur2.rb
+++ imageblur2.rb
@@ -1,39 +1,54 @@
-@image = [[0, 0, 0, 0],
+def blur(image) 
+  blurred_image =   [[0, 0, 0, 0],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0],
+          [0, 0, 0, 0]]
+
+          total_elements = blurred_image.flatten.count
+          rows = total_elements/blurred_image[0].count
+          cells = total_elements/rows
+ 
+      image.each_with_index do |row, row_count|
+        row.each_with_index do |cell_contents, location|
+          if image[row_count][location] == 1 
+            blurred_image[row_count][location] = 1
+            if row_count - 1 >= 0 && row_count - 1 < rows #test for upper cell in bounds
+              blurred_image[row_count - 1][location] = 1
+            end
+            if row_count + 1 >= 0 && row_count + 1 < rows #test for lower cell in bounds
+              blurred_image[row_count + 1][location] = 1
+            end
+            if location - 1 >= 0 && location - 1 < cells #test for left cell in bounds
+              blurred_image[row_count][location - 1] = 1
+            end
+            if location + 1 >= 0 && location + 1 < cells #test for right cell in bounds
+              blurred_image[row_count][location + 1] = 1
+            end 
+          end 
+        end 
+      end
+     return blurred_image
+end
+
+def output(image, label)
+  puts ""
+  puts "Label Is: #{label}"
+  puts "_________"
+  image.each do |row|
+    puts row.join
+  end
+end
+
+oski =    [[1, 0, 0, 0],
           [0, 0, 0, 0],
           [0, 0, 0, 0],
           [0, 1, 0, 0],
           [0, 0, 0, 0],
           [0, 0, 0, 0]]
 
-@blur =   [[0, 0, 0, 0],
-          [0, 0, 0, 0],
-          [0, 0, 0, 0],
-          [0, 0, 0, 0],
-          [0, 0, 0, 0],
-          [0, 0, 0, 0]]
 
-   @image.each_with_index do |row, row_count|
-      row.each_with_index do |cell_contents, location|
-          if @image[row_count][location] == 1 
-            @blur[row_count][location] = 1
-            @blur[row_count + 1][location] = 1
-            @blur[row_count - 1][location] = 1
-            @blur[row_count][location + 1] = 1
-            @blur[row_count][location - 1] = 1
-         end
-       end
-     end
-
-puts ""
-puts "Image Is:"
-puts "_________"
-@image.each do |row|
-  puts row.join
-end
-
-puts ""
-puts "Blur is:"
-puts "_________"
-@blur.each do |row|
-  puts row.join
-end
+blurred_oski = blur(oski)
+output(oski, "Oski")
+output(blurred_oski, "Blurred Oski")
